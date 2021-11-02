f18aeb029fcacbd2ac0683783f39d31ffaa628a3
jincrates <94jingyu@gmail.com>
2021-11-02 21:22:47 +0900

    signup v1, v2 구분

--- src/main/java/com/jincrates/bolierplatelogin/controller/UserController.java
+++ src/main/java/com/jincrates/bolierplatelogin/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.jincrates.bolierplatelogin.controller;
 
+import com.jincrates.bolierplatelogin.dto.ResponseDTO;
 import com.jincrates.bolierplatelogin.dto.UserDTO;
 import com.jincrates.bolierplatelogin.entity.UserEntity;
 import com.jincrates.bolierplatelogin.service.UserService;
@@ -24,8 +25,38 @@ public class UserController {
 
     private final UserService service;
 
-    @PostMapping(path = "/signup", produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO) {
+    @PostMapping(path = "/v1/signup", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<?> registerUserV1(@RequestBody UserDTO userDTO) {
+        try {
+            //요청을 이용해 저장할 사용자 만들기
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+
+            // 서비스를 이용해 리포지터리에 사용자 저장
+            UserEntity registerUser = service.create(userDTO);
+
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(registerUser.getEmail())
+                    .id(registerUser.getId())
+                    .username(registerUser.getUsername())
+                    .build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error(e.getMessage())
+                    .build();
+
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @PostMapping(path = "/v2/signup", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<UserDTO> registerUserV2(@RequestBody UserDTO userDTO) {
 
         UserEntity registerUser = service.create(userDTO);
 
---
+++ src/main/java/com/jincrates/bolierplatelogin/dto/ResponseDTO.java
@@ -0,0 +1,14 @@
+package com.jincrates.bolierplatelogin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO {
+    private String error;
+}
